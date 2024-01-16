78bcc3ce25a8b52a71f1eb238498d2d4e460d85c
kusaba-haruka <151444672+kusaba-haruka@users.noreply.github.com>
2024-01-16 16:40:49 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,20 @@
+ // 配列の宣言と値の代入を行う
+ const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
+
+// 配列の値を出力する
+console.log(holidays);
+
+
+let index = 0;
+
+while (index < holidays.length) {
+  console.log(holidays[index]);
+  index++;
+}
+
+// 1～10までの数値を順番に出力する
+ for (let holidays = 1; holidays <= 16; holidays++) {
+    console.log(holidays);
+  }
+ 
+ 
